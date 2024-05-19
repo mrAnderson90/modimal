26fc7c8caec7f43c209eb0e0929e168377552cff
mrAnderson90 <115795889+mrAnderson90@users.noreply.github.com>
2024-05-19 18:02:22 +0300

    Add src

---
+++ src/js/index.js
@@ -0,0 +1,32 @@
+// import Swiper bundle with all modules installed
+import Swiper from 'swiper/bundle';
+
+// import styles bundle
+import 'swiper/css/bundle';
+
+const swiper = new Swiper('.swiper', {
+  // slidesPerView: 3,
+  slidesPerView: 2,
+  spaceBetween: 16,
+  // loop: true,
+
+	breakpoints: {
+		780: {
+			slidesPerView: 3,
+			spaceBetween: 24,
+		},
+	},
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+// Мобильная навигация
+import mobileNav from './modules/mobile-nav.js';
+mobileNav();
+
+// Aккордеон для мобильной навигации
+import accordion from './modules/accordion.js';
+accordion();
